86f2f5b93ff55f9c3a6a82e8d1f0ef99aab50ff9
Moises Saul Perez Rodriguez <mperez@americas.phoenixcontact.com>
2023-03-16 21:33:26 -0600

    Setting the bases for models

---
+++ models/Receipe.js
@@ -0,0 +1,42 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Receipe extends Model {}
+
+Receipe.init(
+    {
+       id :{
+        type : DataTypes.INTEGER,
+        allowNull : false,
+        autoIncrement : true,
+        primaryKey: true
+       },
+       ingredients :{
+        type : DataTypes.TEXT('long'),
+        allowNull : false
+       },
+       steps :{
+        type : DataTypes.TEXT('long'),
+        allowNull : false 
+       },
+       video_link : {
+        type : DataTypes.TEXT('medium'),
+        allowNull : false 
+       },
+       food_id :{
+        type: DataTypes.INTEGER,
+        references :{
+            model : "food",
+            key : "id"
+        }
+       }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'receipe'
+    }
+);
+
+module.exports = Receipe;
