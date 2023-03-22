df5ab388420d981cca8ff570616948a2d7a1e258
Raul <raulmayas20@gmail.com>
2023-03-21 21:35:10 -0600

    Handling different views

--- models/User.js
+++ models/User.js
@@ -16,6 +16,10 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
+    is_superUser: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
     firstname: {
       type: DataTypes.STRING,
       allowNull: false,
--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
-const routes = require('./routes');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({})
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// turn on routes
-app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/indexRoute'));
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+// app.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
+// Starts the server to begin listening
+app.listen(PORT, () => {
+  console.log('Server listening on: http://localhost:' + PORT);
 });
