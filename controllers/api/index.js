218686518352a03ccb521c6313a3aa92a6406753
Moises Saul Perez Rodriguez <mperez@americas.phoenixcontact.com>
2023-03-23 21:33:37 -0600

    setting the food cards handlevars and partials, also editing the controllers to add the route for food

--- controllers/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-const indexRoute = require("./indexRoute");
-
-router.use("/user", indexRoute);
-
-module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 
-const userRoute = require("./api");
 const homeRoutes = require("./homeRoutes");
 
 router.use("/", homeRoutes);
-router.use("/api", userRoute);
 
 module.exports = router;
