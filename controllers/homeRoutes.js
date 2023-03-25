c9094d6291c262be658bbae7626be626147948f1
Raul <raulmayas20@gmail.com>
2023-03-25 01:07:01 -0600

    Setting random 3 dishes as index

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,20 +4,29 @@ const { Food } = require("../models");
 router.get("/", async (req, res) => {
   try {
     const dbFood = await Food.findAll();
-
+    const foodList = [];
+    let past_value;
+    let new_value;
     let foods = dbFood.map((gallery) => gallery.get({ plain: true }));
-    foods = foods.slice(0, 3);
-    console.log(foods);
+    for (let i = 0; i < 3; i++) {
+      new_value = Math.floor(Math.random() * foods.length);
+      console.log(new_value)
+      if (new_value === past_value) {
+        i--
+      } else {
+        foodList.push(foods[new_value])
+        past_value = new_value;
+      }
+    }
+    console.log(foodList)
     res.render("index", {
-      foods,
+      foodList,
     });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
-
-
 
 router.get("/food/:id", (req, res) => {});
 
