8b7a4527b1233bea88bbc70ff9388160cfca9282
Raul <raulmayas20@gmail.com>
2023-03-26 00:10:10 -0600

    Countries cards done

--- seeds/country-seeds.js
+++ seeds/country-seeds.js
@@ -7,6 +7,7 @@ const countryData = [
     description:
       "Italian food is a delicious and diverse cuisine that features fresh ingredients, pasta, pizza, cheeses, meats, seafood, and a rich culinary history.",
     background_image: "Italy.png",
+    front_image: "italyFront.png",
   },
   {
     name: "Greece",
@@ -14,6 +15,7 @@ const countryData = [
     description:
       "Greek food is fresh and healthy, featuring grilled meats, seafood, vegetables, feta cheese, yogurt, olives, and traditional dishes like moussaka and spanakopita.",
     background_image: "Greece.png",
+    front_image: "greeceFront.png",
   },
   {
     name: "Mexico",
@@ -21,6 +23,7 @@ const countryData = [
     description:
       "Mexican food is bold and flavorful, featuring spicy chilies, savory meats, beans, rice, and tortillas, often accompanied by guacamole and salsa.",
     background_image: "Mexico.png",
+    front_image: "mexicoFront.png",
   },
   {
     name: "France",
@@ -28,6 +31,7 @@ const countryData = [
     description:
       "French food is elegant and refined, featuring buttery sauces, delicate pastries, cheeses, meats, and seafood, and paired with fine wines.",
     background_image: "France.png",
+    front_image: "franceFront.png",
   },
   {
     name: "Peru",
@@ -35,6 +39,7 @@ const countryData = [
     description:
       "Peruvian food is diverse and flavorful, featuring fresh seafood, spicy peppers, potatoes, corn, and unique dishes like ceviche and lomo saltado.",
     background_image: "Peru.png",
+    front_image: "peruFront.png",
   },
   {
     name: "Denmark",
@@ -42,6 +47,7 @@ const countryData = [
     description:
       "Danish food is simple and hearty, featuring open-faced sandwiches, pickled herring, meatballs, fish dishes, and pastries like wienerbrød.",
     background_image: "Denmark.png",
+    front_image: "denmarkFront.png",
   },
 ];
 
