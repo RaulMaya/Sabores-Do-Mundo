9723cb100e831a1129376be27ec301bf1166a712
Raul <raulmayas20@gmail.com>
2023-03-25 22:26:50 -0600

    Log Out functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -24,3 +24,15 @@ router.post("/", async (req, res) => {
 });
 
 module.exports = router;
+
+
+// Logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
