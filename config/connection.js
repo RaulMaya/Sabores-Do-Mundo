cf6d6a0543f42f10d97ec7f3bd3266e0f2c7e0ac
Raul <raulmayas20@gmail.com>
2023-03-27 21:24:08 -0600

    Deployment

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  "heroku_7cd031fc8fceca3",
+  "b44f9ba0468c5d",
+  "725b01d7",
   {
-    host: 'localhost',
+    host: 'us-cdbr-east-06.cleardb.net',
     dialect: 'mysql',
     port: 3306,
   }
